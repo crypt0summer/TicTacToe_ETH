25b5fdbe546b3031a7c003885ca2bc9683b391e8
April <realapril25@gmail.com>
2022-06-08 13:07:11 +0900

    Add function : takeTurn, add test

--- contracts/TicTacToe.sol
+++ contracts/TicTacToe.sol
@@ -37,7 +37,7 @@ contract TicTacToe {
     }
 
     mapping(uint256 => Game) games;
-    
+
     event LogGameId(uint256 gameId);
 
     function createGame() external payable returns (uint256) {
@@ -46,7 +46,7 @@ contract TicTacToe {
 
         games[gameId] = Game({
             id: gameId,
-            turnsTaken: 0,
+            turnsTaken: 0, //9가 되면 게임 끝
             winner: address(0x0),
             lastPlayed: address(0x0),
             user1: Player({addr: payable(msg.sender), betEth: msg.value}),
@@ -56,7 +56,7 @@ contract TicTacToe {
         });
         _gameId.increment();
         emit LogGameId(gameId);
-        
+
         return gameId;
     }
 
@@ -64,6 +64,80 @@ contract TicTacToe {
         Game storage game = games[gameId];
         require(game.user1.betEth == msg.value, "Invalid ETH");
         game.user2 = Player({addr: payable(msg.sender), betEth: msg.value});
+        game.status = GameState.PLAYING;
     }
 
+    modifier checkPlayable(
+        uint256 gameId,
+        uint256 _x,
+        uint256 _y
+    ) {
+        Game memory game = games[gameId];
+        require(game.status == GameState.PLAYING, "Game is not playing");
+        require(
+            msg.sender == game.user1.addr || msg.sender == game.user2.addr,
+            "Not a valid player"
+        );
+        require(game.lastPlayed != msg.sender, "Not your turn");
+        require(0 < _x && _x < 4 && 0 < _y && _y < 4, "Wrong position");
+        _;
+    }
+
+    function takeTurn(
+        uint256 gameId,
+        uint256 _x,
+        uint256 _y
+    ) external checkPlayable(gameId, _x, _y) {
+        Game storage game = games[gameId];
+
+        // Calculate the board location
+        uint256 boardLocation = (_y - 1) * 3 + (_x - 1);
+
+        require(game.board[boardLocation] == BoardState.EMPTY, "Already taken");
+
+        // Mark that the game advanced
+        game.lastPlayed = msg.sender;
+        game.turnsTaken++;
+
+        // Save game
+        BoardState identifier;
+        msg.sender == game.user1.addr
+            ? identifier = BoardState.USER1
+            : identifier = BoardState.USER2;
+        game.board[boardLocation] = identifier;
+
+        // console.log(uint(game.board[0]));
+        // console.log(uint(game.board[1]));
+        // console.log(uint(game.board[2]));
+        // console.log(uint(game.board[3]));
+        // console.log(uint(game.board[4]));
+        // console.log(uint(game.board[5]));
+        // console.log(uint(game.board[6]));
+        // console.log(uint(game.board[7]));
+        // console.log(uint(game.board[8]));
+
+        //Check if the game has ended
+
+        //Draw
+        if (game.turnsTaken == 9) {
+            //TODO check if someone won first
+            game.status = GameState.FINISHED;
+            _resetGame(game);
+        }
+    }
+
+    function _resetGame(Game storage game) private {
+        BoardState[9] memory board;
+        game.turnsTaken = 0;
+        game.board = board;
+        game.status = GameState.PLAYING;
+    }
+
+    function getBoard(uint256 gameId)
+        external
+        view
+        returns (BoardState[9] memory)
+    {
+        return games[gameId].board;
+    }
 }
--- test/tictactoe_test.js
+++ test/tictactoe_test.js
@@ -2,12 +2,12 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("TicTacToe", function () {
-  let account1, account2;
+  let account1, account2, account3;
   let TicTacToe, ttt;
   let gameId;
 
   beforeEach(async () => {
-    [account1, account2] = await ethers.getSigners();
+    [account1, account2, account3] = await ethers.getSigners();
     TicTacToe = await ethers.getContractFactory("TicTacToe");
     ttt = await TicTacToe.deploy();
     await ttt.deployed();
@@ -19,17 +19,17 @@ describe("TicTacToe", function () {
     const receipt = await transaction.wait();
     for (const event of receipt.events) {
       // console.log(`Event ${event.event} with args ${event.args}`);
-      if (event.event == "LogGameId") {
-        gameId = event.args.gameId;
-      }
+
+      gameId = event.args.gameId;
     }
   });
 
   describe("Success Plan", function () {
     it("Create and Start game", async function () {
-      let tx = await ttt.connect(account2).joinAndStartGame(gameId, {value:ethers.utils.parseEther("0.1") });
+      let tx = await ttt
+        .connect(account2)
+        .joinAndStartGame(gameId, { value: ethers.utils.parseEther("0.1") });
       expect(tx).to.not.be.undefined;
-      
     });
     // it("Should User 1 Win", async function () {
     //   expect(1).to.equal(1);
@@ -49,12 +49,46 @@ describe("TicTacToe", function () {
       ).to.be.revertedWith("Invalid ETH");
     });
 
-    // it("Should fail - tried playing twice", async function () {
-    //   expect(1).to.equal(1);
-    // });
+    it("Should fail - tried to play game that wasn't started", async function () {
+      await expect(
+        ttt.connect(account2).takeTurn(gameId, 1, 1)
+      ).to.be.revertedWith("Game is not playing");
+    });
 
-    // it("Should fail - tried taken spot", async function () {
-    //   expect(1).to.equal(1);
-    // });
+    it("Should fail - non player tried to play", async function () {
+      await ttt.connect(account2).joinAndStartGame(gameId, {value:ethers.utils.parseEther("0.1") });
+      await expect(
+        ttt.connect(account3).takeTurn( gameId, 1, 2)
+      ).to.be.revertedWith("Not a valid player");
+    });
+
+    it("Should fail - tried playing twice", async function () {
+      await ttt.connect(account2).joinAndStartGame(gameId, {value:ethers.utils.parseEther("0.1") });
+      await ttt.connect(account2).takeTurn(gameId, 1, 1);
+      await expect(
+        ttt.connect(account2).takeTurn( gameId, 1, 2)
+      ).to.be.revertedWith("Not your turn");
+    });
+
+    it("Should fail - tried wrong spot range", async function () {
+      await ttt.connect(account2).joinAndStartGame(gameId, {value:ethers.utils.parseEther("0.1") });
+      await expect(
+        ttt.connect(account2).takeTurn( gameId, 0, 0)
+      ).to.be.revertedWith("Wrong position");
+      await expect(
+        ttt.connect(account2).takeTurn( gameId, 1, 4)
+      ).to.be.revertedWith("Wrong position");
+      await expect(
+        ttt.connect(account2).takeTurn( gameId, 4, 2)
+      ).to.be.revertedWith("Wrong position");
+    });
+
+    it("Should fail - tried to get spot already taken", async function () {
+      await ttt.connect(account2).joinAndStartGame(gameId, {value:ethers.utils.parseEther("0.1") });
+      await ttt.connect(account2).takeTurn(gameId, 1, 1);
+      await expect(
+        ttt.connect(account1).takeTurn( gameId, 1, 1)
+      ).to.be.revertedWith("Already taken");
+    });
   });
 });
